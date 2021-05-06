cc3df01062d7685861197fc16c0a404299b5020c
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-05-06 13:11:56 +0900

    babel setup

---
+++ server.js
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "baby",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));

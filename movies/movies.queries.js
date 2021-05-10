4f5d9b998d18f4eecd6712b5690ab3aa66af5192
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-05-10 16:28:50 +0900

    commit

--- movies/movies.queries.js
+++
@@ -1,8 +0,0 @@
-import client from "../client";
-
-export default {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
-  },
-};

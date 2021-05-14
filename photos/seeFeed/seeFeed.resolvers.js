5ac163a5c79a6dab5be3cc238eb71c4ede8e0c35
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-05-14 18:11:18 +0900

    until seeFeed

---
+++ photos/seeFeed/seeFeed.resolvers.js
@@ -0,0 +1,33 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeFeed: protectedResolver(async (_, { lastId }, { loggedInUser }) =>
+      client.photo.findMany({
+        where: {
+          OR: [
+            {
+              user: {
+                followers: {
+                  some: {
+                    id: loggedInUser.id,
+                  },
+                },
+              },
+            },
+            {
+              userId: loggedInUser.id,
+            },
+          ],
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: 5,
+        skip: lastId ? 1 : 0,
+        ...(lastId && { cursor: { id: lastId } }),
+      })
+    ),
+  },
+};

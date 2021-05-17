49f471109218b388f17197efe5b5107fa36c5d7b
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-05-17 13:18:14 +0900

    AWS WIP

---
+++ shared/shared.utils.js
@@ -0,0 +1,23 @@
+import AWS from "aws-sdk";
+
+AWS.config.update({
+  credentials: {
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECREY,
+  },
+});
+
+export const uploadPhoto = async (file, userId) => {
+  const { filename, createReadStream } = await file;
+  const readStream = createReadStream();
+  const objectName = `${userId}-${Date.now()}-${filename}`;
+  const { Location } = await new AWS.S3()
+    .upload({
+      Bucket: "instaclone-uploads",
+      Key: objectName,
+      ACL: "public-read",
+      Body: readStream,
+    })
+    .promise();
+  return Location;
+};
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,6 +2,7 @@ import client from "../../client";
 import bcrypt from "bcrypt";
 import { protectedResolver } from "../users.utils";
 import { createWriteStream } from "fs";
+import { uploadPhoto } from "../../shared/shared.utils";
 
 const resolverFn = async (
   _,
@@ -10,20 +11,21 @@ const resolverFn = async (
 ) => {
   let avatarUrl = null;
   if (avatar) {
-    // avatar에서 filename, createReadStream 호출
-    const { filename, createReadStream } = await avatar;
-    console.log(filename);
-    // unique한 이름 생성
-    const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
-    //파일 가져오기 createReadStream()
-    const readStream = createReadStream();
-    //보내줄 경로 정의
-    const writeStream = createWriteStream(
-      process.cwd() + "/uploads/" + newFilename
-    );
-    //파일을 지정해둔 경로로 연결
-    readStream.pipe(writeStream);
-    avatarUrl = `http://localhost:4000/static/${newFilename}`;
+    avatarUrl = await uploadPhoto(avatar, loggedInUser.id);
+    // //avatar에서 filename, createReadStream 호출
+    // const { filename, createReadStream } = await avatar;
+    // console.log(filename);
+    // // unique한 이름 생성
+    // const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+    // //파일 가져오기 createReadStream()
+    // const readStream = createReadStream();
+    // //보내줄 경로 정의
+    // const writeStream = createWriteStream(
+    //   process.cwd() + "/uploads/" + newFilename
+    // );
+    // //파일을 지정해둔 경로로 연결
+    // readStream.pipe(writeStream);
+    // avatarUrl = `http://localhost:4000/static/${newFilename}`;
   }
 
   let uglyPassword = null;

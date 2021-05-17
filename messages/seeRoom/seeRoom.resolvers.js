117e98f99acea6e6031da2d99bfdb78f5736e0e5
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-05-17 16:50:01 +0900

    seeRoom

---
+++ messages/seeRoom/seeRoom.resolvers.js
@@ -0,0 +1,12 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeRoom: protectedResolver(async (_, { id }, { loggedInUser }) => {
+      await client.room.findFirst({
+        where: { id, users: { some: { id: loggedInUser.id } } },
+      });
+    }),
+  },
+};
--- messages/seeRoom/seeRoom.typeDefs.js
+++ messages/seeRoom/seeRoom.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-express";
 
 export default gql`
   type Query {
-    seeRooms: [Room]
+    seeRoom(id: Int): Room
   }
 `;

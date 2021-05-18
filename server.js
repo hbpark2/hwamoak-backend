e53e08f0b2eeb0383a0a62c243ea318bed0f41d2
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-05-18 11:34:06 +0900

    until RealTine

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import http from "http";
 import express from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
@@ -11,10 +12,30 @@ const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   uploads: false, // 추가
-  context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-    };
+  context: async (ctx) => {
+    if (ctx.req) {
+      return {
+        loggedInUser: await getUser(ctx.req.headers.token),
+      };
+    } else {
+      const {
+        connection: { context },
+      } = ctx;
+      return {
+        loggedInUser: context.loggedInUser,
+      };
+    }
+  },
+  subscriptions: {
+    onConnect: async ({ token }) => {
+      if (!token) {
+        throw new Error("You can't listen.");
+      }
+      const loggedInUser = await getUser(token);
+      return {
+        loggedInUser,
+      };
+    },
   },
 });
 
@@ -23,9 +44,17 @@ app.use(graphqlUploadExpress()); // 추가
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
-app.listen({ port: PORT }, () => {
+
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(` 🌗  Server is running on http://localhost:${PORT}/ 🌗  `);
 });
+
+// app.listen({ port: PORT }, () => {
+//   console.log(` 🌗  Server is running on http://localhost:${PORT}/ 🌗  `);
+// });
 
 // .then(() =>
 //   console.log(` 🌗  Server is running on http://localhost:${PORT}/`)

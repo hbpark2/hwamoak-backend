f3b5fef1e9f853c5cab3d5684157d86c3f0624b2
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-06-20 23:49:07 +0900

    introspection

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   typeDefs,
   resolvers,
+  playground: true,
+  introspection: true,
   uploads: false, // 추가
   context: async (ctx) => {
     if (ctx.req) {

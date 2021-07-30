8a12a52ca2130685a2d9ee3a69c0b03d2331ac28
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-07-30 10:06:29 +0900

    delete azure

--- src/shared/shared.utils.js
+++ src/shared/shared.utils.js
@@ -1,37 +1,37 @@
 // import { BlobServiceClient } from "@azure/storage-blob";
 // import azure from "azure-storage";
-// import AWS from "aws-sdk";
-// import { createWriteStream } from "fs";
-// AWS.config.update({
-//   credentials: {
-//     accessKeyId: process.env.AWS_KEY,
-//     secretAccessKey: process.env.AWS_SECRET,
-//   },
-// });
+import AWS from "aws-sdk";
+import { createWriteStream } from "fs";
+AWS.config.update({
+  credentials: {
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
 
-// export const uploadToS3 = async (file, userId, folderName) => {
-//   const { filename, createReadStream } = await file;
-//   const readStream = createReadStream();
-//   const objectName = `${folderName}/${userId}-${Date.now()}-${filename}`;
-//   const { Location } = await new AWS.S3()
-//     .upload({
-//       Bucket: "hwamoak",
-//       Key: objectName,
-//       ACL: "public-read",
-//       Body: readStream,
-//     })
-//     .promise();
-//   return Location;
-// };
+export const uploadToS3 = async (file, userId, folderName) => {
+  const { filename, createReadStream } = await file;
+  const readStream = createReadStream();
+  const objectName = `${folderName}/${userId}-${Date.now()}-${filename}`;
+  const { Location } = await new AWS.S3()
+    .upload({
+      Bucket: "hwamoak",
+      Key: objectName,
+      ACL: "public-read",
+      Body: readStream,
+    })
+    .promise();
+  return Location;
+};
 
-// //AWS에서 사진 삭제
-// export const deleteInS3 = async (fileUrl) => {
-//   const Key = fileUrl.replace("https://hwamoak.s3.amazonaws.com/", "");
-//   await S3.deleteObject({
-//     Bucket,
-//     Key,
-//   }).promise();
-// };
+//AWS에서 사진 삭제
+export const deleteInS3 = async (fileUrl) => {
+  const Key = fileUrl.replace("https://hwamoak.s3.amazonaws.com/", "");
+  await S3.deleteObject({
+    Bucket,
+    Key,
+  }).promise();
+};
 
 // const blobService = azure.createBlobService(process.env.AZURE_STORAGE_KEY);
 

8adf363ede0f9aa8cb9019038f93c1c968a4f3be
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-08-18 12:50:37 +0900

    seeFeedQuery

--- src/photos/seeFeed/seeFeed.resolvers.js
+++ src/photos/seeFeed/seeFeed.resolvers.js
@@ -4,10 +4,10 @@ import { protectedResolver } from "../../users/users.utils";
 export default {
   Query: {
     seeFeed: protectedResolver(async (_, { offset }, { loggedInUser }) => {
-      // console.log(offset);
+      console.log(offset);
       return client.photo.findMany({
         take: 3,
-        skip: offset,
+        skip: offset || 0,
         // where: {
         //   OR: [
         //     {

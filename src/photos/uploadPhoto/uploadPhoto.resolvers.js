e18391c82f5de3476d352ae979c39becea6fdfa9
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-08-23 17:50:50 +0900

    upload photo

--- src/photos/uploadPhoto/uploadPhoto.resolvers.js
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -3,25 +3,59 @@ import { UploadToAzure, uploadToS3 } from "../../shared/shared.utils";
 import { protectedResolver } from "../../users/users.utils";
 import { processHashtags } from "../photos.utils";
 
+// export default {
+//   Mutation: {
+//     uploadPhoto: protectedResolver(
+//       async (_, { file, caption }, { loggedInUser, headers }) => {
+//         let hashtagObj = [];
+//         /// parse caption
+//         if (caption) {
+//           hashtagObj = processHashtags(caption);
+//         }
+//         // console.log("headers : ", headers["content-length"]);
+//         const fileUrl = await uploadToS3(file, loggedInUser.id, "uploads");
+//         // const fileUrl = await UploadToAzure(
+//         //   file,
+//         //   loggedInUser.id,
+//         //   "uploads",
+//         //   headers
+//         // );
+
+//         // get or create Hashtags
+//         return client.photo.create({
+//           data: {
+//             file: fileUrl,
+//             caption,
+//             user: {
+//               connect: {
+//                 id: loggedInUser.id,
+//               },
+//             },
+//             ...(hashtagObj.length > 0 && {
+//               hashtags: {
+//                 connectOrCreate: hashtagObj,
+//               },
+//             }),
+//           },
+//         });
+
+//         // save the photo WITH the parsed hashtags
+//         // add the photo to the hashtags
+//       }
+//     ),
+//   },
+// };
+
+
 export default {
   Mutation: {
     uploadPhoto: protectedResolver(
-      async (_, { file, caption }, { loggedInUser, headers }) => {
+      async (_, { file, caption }, { loggedInUser }) => {
         let hashtagObj = [];
-        /// parse caption
         if (caption) {
           hashtagObj = processHashtags(caption);
         }
-        // console.log("headers : ", headers["content-length"]);
-        // const fileUrl = await uploadToS3(file, loggedInUser.id, "uploads");
-        const fileUrl = await UploadToAzure(
-          file,
-          loggedInUser.id,
-          "uploads",
-          headers
-        );
-
-        // get or create Hashtags
+        const fileUrl = await uploadToS3(file, loggedInUser.id, "uploads");
         return client.photo.create({
           data: {
             file: fileUrl,
@@ -38,9 +72,6 @@ export default {
             }),
           },
         });
-
-        // save the photo WITH the parsed hashtags
-        // add the photo to the hashtags
       }
     ),
   },
--- src/users/me/me.resolvers.js
+++ src/users/me/me.resolvers.js
@@ -3,13 +3,12 @@ import { protectedResolver } from "../users.utils";
 
 export default {
   Query: {
-    me: protectedResolver(
-      async (_, __, { loggedInUser }) =>
-        await client.user.findUnique({
-          where: {
-            id: loggedInUser.id,
-          },
-        })
+    me: protectedResolver((_, __, { loggedInUser }) =>
+      client.user.findUnique({
+        where: {
+          id: loggedInUser.id,
+        },
+      })
     ),
   },
 };

eecbc06a0686836fcf826954331e34932cf8df18
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-09-02 09:44:52 +0900

    delete conosole

--- src/messages/findRoom/findRoom.typeDefs.js
+++ src/messages/findRoom/findRoom.typeDefs.js
@@ -1,12 +1,6 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-  # type findRoomResult {
-  #   ok: Boolean!
-  #   error: String
-  #   room: Room
-  # }
-
   type Query {
     findRoom(talkingToId: Int): MutationResponse!
   }
